73a7d6445001f016673453fb0b346fc8d9387f31
Pranav C <pranavchavez@gmail.com>
2023-05-09 22:45:22 +0530

    Netflix

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img className='logo' src='https://farm6.staticflickr.com/5821/20639706793_8c038faa4a_o.png'/>
+    </div>
+  )
+}
+
+export default Nav
